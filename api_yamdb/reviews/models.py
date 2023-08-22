f21425718c8d5aa01bdd458a98d29120c4bb31b6
IllarionovP <faraon4ik2020@mail.ru>
2023-08-23 00:36:38 +0300

    Second commit in models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,39 +8,21 @@ User = get_user_model()
 class Review(models.Model):
     """Отзывы пользователей и их контент."""
 
-    SCORE_CHOICES = (
-        (1, '1. Очень плохо. Не понравилось совсем.'),
-        (2, '2. Плохо. Не понравилось почти всё.'),
-        (3, '3. Не очень. Не понравилось многое.'),
-        (4, '4. Так себе. Мало что понравилось.'),
-        (5, '5. Ни то, ни сё. Почти ничего не понравилось.'),
-        (6, '6. Неплохо. Кое-что понравилось.'),
-        (7, '7. Хорошо. Многое понравилось.'),
-        (8, '8. Очень хорошо. Почти всё понравилось.'),
-        (9, '9. Великолепно. Очень понравилось.'),
-        (10, '10. Высший балл. В восторге.'),
-    )
     title = models.ForeignKey(
         Title, on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Произведение'
+        related_name='reviews'
     )
     text = models.TextField(
-        max_length=6000,
-        verbose_name='Текст отзыва'
+        max_length=6000
     )
     author = models.ForeignKey(
         User, on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Автор отзыва'
+        related_name='reviews'
     )
     score = models.SmallIntegerField(
-        choices=SCORE_CHOICES,
-        verbose_name='Оценка произведения пользователем'
     )
     pub_date = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата создания отзыва'
+        auto_now_add=True
     )
 
     class Meta:
@@ -52,21 +34,17 @@ class Comment(models.Model):
 
     review = models.ForeignKey(
         Review, on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Комментируемый отзыв'
+        related_name='comments'
     )
     text = models.TextField(
-        max_length=1500,
-        verbose_name='Текст комментария'
+        max_length=1500
     )
     author = models.ForeignKey(
         User, on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор комментария'
+        related_name='comments'
     )
     pub_date = models.DateTimeField(
-        default=timezone.now,
-        verbose_name='Дата создания комментария'
+        auto_now_add=True
     )
 
     class Meta:
