6078a98eec60f27e065e69374328c87a2db1d05f
Andey Dogadkin <jvgger@gmail.com>
2023-08-24 05:17:13 +0300

    added management command load_csv

--- api_yamdb/reviews/management/commands/load_csv.py
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -13,20 +13,27 @@ class Command(BaseCommand):
     MODELS_FILE_NAMES = {Category: 'category', Genre: 'genre',
                          Title: 'titles', GenreTitle: 'genre_title',
                          User: 'users', Review: 'review', Comment: 'comments'}
+    USER_ROLES = {'user': 1, 'moderator': 2, 'admin': 3}
 
     def handle(self, *args, **options):
         for model, file_name in self.MODELS_FILE_NAMES.items():
             self.__add_csv_files_to_db__(model, file_name)
 
-    @staticmethod
-    def __add_csv_files_to_db__(model, file_name: str):
+    @classmethod
+    def __add_csv_files_to_db__(cls, model, file_name: str):
         with open(f'static/data/{file_name}.csv') as csvfile:
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 row: dict = row
                 if model is Title:
-                    category = Category.objects.get(id=row.pop('category'))
-                    model.objects.create(**row, category=category)
+                    category = row.pop('category')
+                    model.objects.create(**row, category_id=category)
+                elif model is User:
+                    role = cls.USER_ROLES.get(row.pop('role'))
+                    model.objects.create(**row, role=role)
+                elif model in (Review, Comment):
+                    author = row.pop('author')
+                    model.objects.create(**row, author_id=author)
                 else:
                     model.objects.create(**row)
             csvfile.close()
